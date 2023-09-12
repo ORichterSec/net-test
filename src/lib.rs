e1d43bf91a54b8141bcb9b628f1c0abf00c5e2f9
Oliver Richter <oliver.richter@secunet.com>
2023-09-12 12:19:14 +0200

    update 12_09_2023

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,7 @@ pub mod service{
         address: IpAddr,
         port: u16,
         protocoll: Protocoll,
+        sample: String,
     }
 
     impl Config{
@@ -47,10 +48,11 @@ pub mod service{
             let address = config.get(section, "address").unwrap();
             let port = config.get(section, "port").unwrap();
             let protocoll = config.get(section, "protocoll").unwrap();
-            Config::parse_string_triple(&address, &port, &protocoll)
+            let sample = config.get(section, "sample").unwrap() + path.as_str();
+            Config::parse_string_ini(&address, &port, &protocoll, &sample)
         }
 
-        fn parse_string_triple(address: &String, port: &String, protocoll: &String) -> Config {
+        fn parse_string_ini(address: &String, port: &String, protocoll: &String, sample: &String) -> Config {
             let address: IpAddr = address.clone().parse().unwrap_or_else( |_| panic!("invalid value for ip address '{}'", address));
             let port = port.clone().parse().unwrap_or_else( |_| panic!("invalid value for port '{}'", port));
             let protocoll =
@@ -59,12 +61,18 @@ pub mod service{
                     "udp" => Protocoll::UDP,
                     _ => Protocoll::None,
                 };
-            Config { address, port, protocoll }
+            let sample = sample.clone().parse().unwrap_or_else(|_| panic!("invalid value for sample '{}'", sample));
+            Config { address, port, protocoll, sample }
         }
 
         pub fn new(args: &[String]) -> Config {
 
             //try to read from file if we pass -f flag as first arg
+            if args.len() == 2{
+                if args[1] == "-f" {
+                    return Config::from_file(&String::from("/etc/conf.ini"));
+                }
+            }
             if args.len() >= 3{
                 if args[1] == "-f" {
                     return Config::from_file(&args[2]);
@@ -77,16 +85,16 @@ pub mod service{
             let addr = &args[1];
             let port = &args[2];
             if args.len() == 4{
-                Config::parse_string_triple(addr, port, &args[3].to_string())
+                Config::parse_string_ini(addr, port, &args[3].to_string(), &String::from("default text"))
             }else{
-                Config::parse_string_triple(addr, port, &"".to_string())
+                Config::parse_string_ini(addr, port, &"".to_string(), &String::from("default text"))
             }
             
         }
 
     }
 
-    fn sent_answer(mut stream: TcpStream) -> Result<String, Box<dyn Error>>{
+    fn sent_answer(mut stream: TcpStream, additional_text: &String) -> Result<String, Box<dyn Error>>{
         let mut read_size;
         let mut write_size = 0;
         loop{
@@ -95,7 +103,7 @@ pub mod service{
             read_size =  stream.read(&mut buf)?;
             println!("content of buf:{}", str::from_utf8(&buf).unwrap());
 
-            let output = format!("got {} bytes from tcp stream wrote {} bytes", read_size, write_size);
+            let output = format!("got {} bytes from tcp stream wrote {} bytes| {}", read_size, write_size, additional_text);
             println!("{}",output);
             if read_size == 0 {
                 break;
@@ -105,12 +113,12 @@ pub mod service{
         Ok(format!("sent answer of {} bytes", write_size))
     }
     
-    fn start_tcp_listener(socket: SocketAddr, timeout: Option<u64>) -> Result<String, Box<dyn Error>> {
+    fn start_tcp_listener(socket: SocketAddr, timeout: Option<u64>, additional_text: &String) -> Result<String, Box<dyn Error>> {
         let start = time::Instant::now();
         let listener = TcpListener::bind(socket)?;
         loop{
             let (stream, _) = listener.accept()?;
-            sent_answer(stream)?;
+            sent_answer(stream, additional_text)?;
             let quit =
                 match timeout {
                     Some(value) => start.elapsed().as_secs() > value,
@@ -129,52 +137,8 @@ pub mod service{
         };
         Ok(format!("closed TCPListener after {} seconds (timeout)",output))
     }
-
-    // fn ready_to_answer(socket: SocketAddr) -> Result<String, Box<dyn Error>>{
-    //     let listener = TcpListener::bind(socket)?;
-    //     for stream in listener.incoming() {
-    //         let stream = stream?;
-    //         sent_answer(stream)?;
-    //     }
-    //     Ok(String::from("pass"))
-    // }
-
-    // fn start_tcp_listener(socket: SocketAddr, timeout: Option<u64>) -> Result<String, Box<dyn Error>> {
-    //     let start = time::Instant::now();
-    //     // loop{
-    //     // let stream = listener.accept();
-    //     let(tx, rx) = mpsc::channel();
-
-        
-    //     let join = thread::spawn(move || tx.send(ready_to_answer(socket).unwrap()));
-    //     let recieved = rx.try_recv();
-    //     match recieved {
-    //         Ok(value) => println!("Recieved from worker:{}", value),
-    //         Err(error_str) => println!("Error while trying to receive: {}", error_str),
-    //     }
-        
-    //     loop{
-    //         thread::sleep(std::time::Duration::new(1, 0));
-    //         let quit =
-    //             match timeout {
-    //                 Some(value) => start.elapsed().as_secs() > value,
-    //                 None => false,
-    //             };
-    //         if quit{
-    //             break;
-    //         }else{
-    //             continue;
-    //         }
-    //     }
-
-    //     let output = match timeout {
-    //         Some(value) => value.to_string(),
-    //         None => String::from("THIS SHOULD NOT HAVE BEEN PRINTED. REPLACE THIS WITH PANIC!/UNWRAP() LATER."),
-    //     };
-    //     Ok(format!("closed TCPListener after {}",output))
-    // }
     
-    fn start_upd_listener(socket: SocketAddr, timeout: Option<u64>) -> Result<String, Box<dyn Error>> {
+    fn start_upd_listener(socket: SocketAddr, timeout: Option<u64>, additional_text: &String) -> Result<String, Box<dyn Error>> {
         let socket = UdpSocket::bind(socket)?;
         // Receives a single datagram message on the socket. If `buf` is too small to hold
         // the message, it will be cut off.
@@ -186,7 +150,7 @@ pub mod service{
             // Redeclare `buf` as slice of the received data and send reverse data back to origin.
             let buf = &mut buf[..amt];
             buf.reverse();
-            println!("{:?} bytes received from {:?}", amt, src);
+            println!("{:?} bytes received from {:?}| {}", amt, src, additional_text);
             let len = socket.send_to(buf, &src)?;
             println!("{:?} bytes sent", len);
 
@@ -209,10 +173,10 @@ pub mod service{
         // the socket is closed here
     }
 
-    fn start_listener(socket: SocketAddr, protocoll: &Protocoll, timeout: Option<u64>) -> Result<String, Box<dyn Error>>{
+    fn start_listener(socket: SocketAddr, protocoll: &Protocoll, timeout: Option<u64>, additional_text: &String) -> Result<String, Box<dyn Error>>{
         match protocoll{
-            Protocoll::TCP => return start_tcp_listener(socket, timeout),
-            Protocoll::UDP => return start_upd_listener(socket, timeout),
+            Protocoll::TCP => return start_tcp_listener(socket, timeout, additional_text),
+            Protocoll::UDP => return start_upd_listener(socket, timeout, additional_text),
             Protocoll::None => println!("invalid choice '{:?}' for protocoll choose either: tcp|udp", protocoll),
         };
         return Ok(String::from("pass"));
@@ -222,9 +186,10 @@ pub mod service{
         let addr = config.address;
         let port = config.port;
         let protocoll = &config.protocoll;
+        let sample = &config.sample;
         let socket_addr: SocketAddr = SocketAddr::from((addr,port));
         
-        return start_listener(socket_addr, protocoll, Some(TIME_OUT));
+        return start_listener(socket_addr, protocoll, Some(TIME_OUT), sample);
 
         // Ok(String::from("pass"))
     }
